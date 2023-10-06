129d0d80e4b1c3c7fba91f38b69871498a85830e
Nguyen Tuan Cuong (K17 HL) <cuongnthe171665@fpt.edu.vn>
2023-10-06 22:32:22 +0700

    Simple Student Management System

---
+++ src/main/java/com/springmvc/studentmanagementsystem/SimpleStudentManagementSystemUsingSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.springmvc.studentmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleStudentManagementSystemUsingSpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleStudentManagementSystemUsingSpringMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springmvc/studentmanagementsystem/SimpleStudentManagementSystemUsingSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmvc.studentmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleStudentManagementSystemUsingSpringMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
